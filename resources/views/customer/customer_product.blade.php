62b91408f87c0080af656b46c66e780785fedcee
Jonlukbong <52275960+Jonlukbong@users.noreply.github.com>
2022-07-22 17:50:59 +0700

    เปลี่ยนสี

--- resources/views/customer/customer_product.blade.php
+++ resources/views/customer/customer_product.blade.php
@@ -1,23 +1,80 @@
 @extends('layouts.customer.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<!-- <div class="col-md-10"> -->
+<div class="card">
+    <div class="card-header">ข้อมูลสินค้า</div>
+    <div class="card-body">
 
-                    จัดการสินค้า
+        <!-- หัวข้อที่ 1 -->
+        <div class="container px-0">
+            <div class="row gx-5">
+                <div class="col">
+                    <div class="p-2 border bg-light">1.จัดการสต็อกสินค้า</div>
                 </div>
             </div>
+            <br>
+            @if(!empty( $update_1))
+            <a class="m-1 font-weight-bold text-primary">{{ $update_1 }}
+                <a href="{{ url('/Q1') }}" title="Back"><button class="btn btn-primary btn-sm" disabled></i>คลิก</button></a>
+                @else
+                <a href="{{ url('/Q1') }}" title="Back"><button class="btn btn-primary btn-sm"></i>คลิก</button></a>
+                @endif
         </div>
+        <br>
+
+        <!-- หัวข้อที่ 2 -->
+        <div class="container px-0">
+            <div class="row gx-5">
+                <div class="col">
+                    <div class="p-2 border bg-light">2.สั่งซื้อสินค้า</div>
+                </div>
+            </div>
+            <br>
+            @if(!empty( $update_3))
+            <a class="m-1 font-weight-bold text-primary">{{ $update_3 }}
+                <a href="{{ url('/Q3') }}" title="Back"><button class="btn btn-primary btn-sm" disabled></i>คลิก</button></a>
+                @else
+                <a href="{{ url('/Q3') }}" title="Back"><button class="btn btn-primary btn-sm"></i>คลิก</button></a>
+                @endif
+        </div>
+        <br>
+
+        <!-- หัวข้อที่ 3 -->
+        <div class="container px-0">
+            <div class="row gx-5">
+                <div class="col">
+                    <div class="p-2 border bg-light">3.ออเดอร์สินค้า</div>
+                </div>
+            </div>
+            <br>
+            @if(!empty( $update_2))
+            <a class="m-1 font-weight-bold text-primary">{{ $update_2 }}
+                <a href="{{ url('/Q2') }}" title="Back"><button class="btn btn-primary btn-sm" disabled></i>คลิก</button></a>
+                @else
+                <a href="{{ url('/Q2') }}" title="Back"><button class="btn btn-primary btn-sm"></i>คลิก</button></a>
+                @endif
+        </div>
+        <br>
+
+        <!-- หัวข้อที่ 4 -->
+        <div class="container px-0">
+            <div class="row gx-5">
+                <div class="col">
+                    <div class="p-2 border bg-light">4.รายรับรายจ่าย</div>
+                </div>
+            </div>
+            <br>
+            @if(!empty( $update_4))
+            <a class="m-1 font-weight-bold text-primary">{{ $update_4 }}
+                <a href="{{ url('/Q3-2') }}" title="Back"><button class="btn btn-primary btn-sm" disabled></i>คลิก</button></a>
+                @else
+                <a href="{{ url('/Q3-2') }}" title="Back"><button class="btn btn-primary btn-sm"></i>คลิก</button></a>
+                @endif
+        </div>
+        <br>
+
     </div>
 </div>
 @endsection
