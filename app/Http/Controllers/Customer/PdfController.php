4a085fb80d4aa414df69e83648226d6a8af1dac8
Jonlukbong <52275960+Jonlukbong@users.noreply.github.com>
2022-11-27 20:15:32 +0700

    27-11-2022

--- app/Http/Controllers/Customer/PdfController.php
+++ app/Http/Controllers/Customer/PdfController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Customer;
+
 use App\Http\Controllers\Controller;
 use App\User;
 use \PDF;
@@ -8,29 +9,43 @@ use App\Models\Finance2;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 class PdfController extends Controller
 {
     public function index(Request $request)
     {
-       
-        return view('finance2.report');
+        $keyword = $request->get('search');
+        $perPage = 25;
+        $user_id = Auth::id();
+
+        if (!empty($keyword)) {
+            $finance2 = Finance2::where('month', 'LIKE', "%$keyword%")
+                ->orWhere('sum2', 'LIKE', "%$keyword%")
+                ->orWhere('user_id', 'LIKE', "%$keyword%")
+                ->where('user_id', '=', $user_id)
+                ->latest()->paginate($perPage);
+        } else {
+            $finance2 = Finance2::where('user_id', '=', $user_id)
+                ->latest()->paginate($perPage);
+        }
+        return view('finance2.report', compact('finance2', 'user_id'));
     }
-    public function report(Request $request ,$id)
+    public function report(Request $request, $id)
     {
-          //ค้นหาตาม pk
-          $users = User::findOrFail($id);
+        //ค้นหาตาม pk
+        $users = User::findOrFail($id);
 
 
-          $finance2 = Finance2::where('user_id','=',$users) 
-          ->latest();
-      
-
-          
-         $compact = compact('users','finance2');
+        $finance2 = Finance2::where('user_id', '=', $users)
+            ->latest();
 
 
-         $pdf = PDF::loadView('finance2.report', $compact);
-         return @$pdf->stream('report.pdf');
+
+        $compact = compact('users', 'finance2');
+
+
+        $pdf = PDF::loadView('finance2.report', $compact);
+        return @$pdf->stream('report.pdf');
 
         // return view('pdf.medic_pdf'  , compact('users'));
     }
--- resources/views/layouts/customer/sidebar.blade.php
+++ resources/views/layouts/customer/sidebar.blade.php
@@ -1,13 +1,12 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
-   <!-- Sidebar -->
-   <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
-  
   <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center"  href="{{ url('/') }}">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
     <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-leaf"></i>
     </div>
-    <div class="sidebar-brand-text mx-3" >🍼Vyadong</div>
+    <div class="sidebar-brand-text mx-3">🍼Vyadong</div>
   </a>
 
   <!-- Divider -->
@@ -20,37 +19,34 @@
 
   <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item">
-            <a class="nav-link" href="{{ url('/customer') }}">
-                <i class="fas fa-circle"></i>
-                <span>หน้าหลัก</span></a>
-        </li>
-  <li class="nav-item">
-            <a class="nav-link" href="{{ url('/customer_branch') }}">
-                <i class="fas fa-circle"></i>
-                <span>จัดการร้าน</span></a>
-        </li>
-  <!-- <li class="nav-item">
-            <a class="nav-link" href="{{ url('/customer_product') }}">
-                <i class="fas fa-circle"></i>
-                <span>ข้อมูลสินค้า</span></a> -->
-                
-                
-                <button class="btn btn-default" type="button" data-toggle="dropdown" style="text-align: left; margin: 2px;">
-                <i class="fas fa-circle"></i>
-                <span style="color: white; font-size: 0.85rem"> ข้อมูลสินค้า</span></button>
-                <ul class="dropdown-menu">
-                  <li><a href="{{ url('/Cus_stock') }}" style="color: black; font-size: 0.85rem">1.จัดการสต็อกสินค้า</a></li>
-                  <li><a href="{{ url('/Cus_buy') }}" style="color: black; font-size: 0.85rem">2.สั่งซื้อสินค้า</a></li>
-                  <li><a href="{{ url('/cus_dealerorder') }}" style="color: black; font-size: 0.85rem">3.ออเดอร์สินค้า</a></li>
-                  <li><a href="{{ url('/finance') }}" style="color: black; font-size: 0.85rem">4.รายรับรายจ่าย</a></li>
-                </ul>
-              
+    <a class="nav-link" href="{{ url('/customer') }}">
+      <i class="fas fa-circle"></i>
+      <span>หน้าหลัก</span></a>
   </li>
   <li class="nav-item">
-            <a class="nav-link" href="{{ url('/customer_contact') }}">
-                <i class="fas fa-circle"></i>
-                <span>ติดต่อแอดมิน</span></a>
+    <a class="nav-link" href="{{ url('/customer_branch') }}">
+      <i class="fas fa-circle"></i>
+      <span>จัดการร้าน</span></a>
   </li>
-     
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+      <i class="fas fa-fw fa-wrench"></i>
+      <span>ข้อมูลสินค้า</span>
+    </a>
+    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a href="{{ url('/Cus_stock') }}" class="collapse-item">1.จัดการสต็อกสินค้า</a>
+        <a href="{{ url('/Cus_buy') }}" class="collapse-item">2.สั่งซื้อสินค้า</a>
+        <a href="{{ url('/cus_dealerorder') }}" class="collapse-item">3.ออเดอร์สินค้า</a>
+        <a href="{{ url('/finance') }}" class="collapse-item">4.รายรับรายจ่าย</a>
+      </div>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{ url('/customer_contact') }}">
+      <i class="fas fa-circle"></i>
+      <span>ติดต่อแอดมิน</span></a>
+  </li>
+
 </ul>
 <!-- End of Sidebar -->
